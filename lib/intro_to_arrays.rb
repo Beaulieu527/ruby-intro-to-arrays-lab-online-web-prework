9a5a51180d5047774eb446524f1f1fb4366cc138
beaulieudev <beaulieudev@gmail.com>
2018-10-11 00:48:24 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,24 +10,7 @@ end
 
 def taylor_swift
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-
-  puts taylor_swift
-end
-
-def first_element
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   taylor_swift[0]
-  return taylor_swift[0]
-end
-
-def third_element
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   taylor_swift[2]
-  puts taylor_swift.inspect
-end
-
-def last_element
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   taylor_swift[-1]
-  puts taylor_swift.inspect
 end
