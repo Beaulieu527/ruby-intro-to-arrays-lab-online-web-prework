696dad859a399a438d6d6afacc4ed812194126c6
beaulieudev <beaulieudev@gmail.com>
2018-10-11 00:41:32 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -16,7 +16,7 @@ end
 def first_element
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   taylor_swift[0]
-  puts taylor_swift.inspect
+  return taylor_swift.inspect
 end
 
 def third_element
