212470ba2c2bdce8389940b63d8ebca292e65e65
beaulieudev <beaulieudev@gmail.com>
2018-10-11 00:50:05 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -15,14 +15,17 @@ end
 def first_element
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   taylor_swift[0]
+  return
 end
 
 def third_element
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   taylor_swift[2]
+  return
 end
 
 def last_element
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   taylor_swift[-1]
+  return
 end
