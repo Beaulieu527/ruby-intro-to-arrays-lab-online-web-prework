3a1a60070d64ea34bbb9067b8bfcede1117e322d
beaulieudev <beaulieudev@gmail.com>
2018-10-11 00:39:23 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -15,15 +15,15 @@ end
 
 def first_element
   taylor_swift[0]
-  puts taylor_swift.inspect
+  puts taylor_swift
 end
 
 def third_element
   taylor_swift[2]
-  puts taylor_swift.inspect
+  puts taylor_swift
 end
 
 def last_element
   taylor_swift[-1]
-  puts taylor_swift.inspect
+  puts taylor_swift
 end
