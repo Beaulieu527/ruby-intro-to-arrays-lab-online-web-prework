5804f9b52f5ba38783b044e076a8d4af69b65d6e
beaulieudev <beaulieudev@gmail.com>
2018-10-11 00:57:39 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -12,19 +12,19 @@ def taylor_swift
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
 end
 
-def first_element
+def first_element(array, index_number)
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   taylor_swift[0]
   return
 end
 
-def third_element
+def third_element(array, index_number)
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   taylor_swift[2]
   return
 end
 
-def last_element
+def last_element(array, index_number)
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   taylor_swift[-1]
   return
