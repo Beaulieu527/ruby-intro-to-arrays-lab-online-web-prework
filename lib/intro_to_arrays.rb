ce1e22afb5b77cbb5f2a537af549f1afa2769b83
beaulieudev <beaulieudev@gmail.com>
2018-10-11 00:33:40 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,15 +10,15 @@ end
 
 def taylor_swift
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  return taylor_swift
+  puts taylor_swift
 end
 
 def first_element
   taylor_swift.first
-  return taylor_swift.inspect
+  puts taylor_swift.inspect
 end
 
 def third_element
   taylor_swift[2]
-  return taylor_swift.inspect
+  puts taylor_swift.inspect
 end
