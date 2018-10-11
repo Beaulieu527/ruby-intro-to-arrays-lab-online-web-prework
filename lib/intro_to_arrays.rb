e5419649e2198b1d6f8285c7d37cb813128b47e7
beaulieudev <beaulieudev@gmail.com>
2018-10-11 01:02:48 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,11 +1,10 @@
 def instantiate_new_array
-  numbers = []
- return numbers
+   []
 end
 
 def array_with_two_elements
-  nubers = [1,2]
-  return nubers
+  array = [1,2]
+  return array
 end
 
 def first_element(array)
